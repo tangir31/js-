923dee6fc709a95ffb2b70bd845c4a6195837399
tangir31 <tangirbaxtiyorov@gmail.com>
2023-10-17 12:52:32 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let name = prompt("Ваше имя");
+if (name.length % 2 === 0) {
+  alert("Добро пожаловать!");
+} else {
+  alert("Сори не подходиш");
+}
+
+let number = prompt("Введите число");
+if (number % 2 === 0) {
+  alert("Да");
+} else {
+  alert("Нет");
+}
+
+const Nom = prompt("Введите ваше имя");
+if (name.charAt(0).toLowerCase() === "a") {
+  alert("Welcome");
+} else {
+  alert("go away");
+}
